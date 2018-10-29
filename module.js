5eb871c04756c4f0026f29bbcd541f4ee14d981c
martinpastore <martinpablopastore@gmail.com>
2018-10-29 16:04:17 -0300

    IMprove

--- module.js
+++ module.js
@@ -1,56 +1,9 @@
 const burger = require('burgerjs');
 
 burger.module([
-    [
-        {
-            name: 'input'
-        },
-        {
-            type: 'component'
-        },
-        {
-            selector: 'p-input'
-        }
-    ],
-    [
-        {
-            name: 'footer'
-        },
-        {
-            type: 'component'
-        },
-        {
-            selector: 'p-footer'
-        }
-    ],
-    [
-        {
-            name: 'header'
-        },
-        {
-            type: 'component'
-        },
-        {
-            selector: 'p-header'
-        }
-    ],
-    [
-        {
-            name: 'spinner'
-        },
-        {
-            type: 'component'
-        },
-        {
-            selector: 'p-spinner'
-        }
-    ],
-    [
-        {
-            name: 'home'
-        },
-        {
-            type: 'page'
-        }
-    ]
+    [{name: 'input'},{type: 'component'},{selector: 'p-input'}],
+    [{name: 'footer'},{type: 'component'},{selector: 'p-footer'}],
+    [{name: 'header'},{type: 'component'},{selector: 'p-header'}],
+    [{name: 'spinner'},{type: 'component'},{selector: 'p-spinner'}],
+    [{name: 'home'},{type: 'page'},{selector: 'p-home'}]
 ]);
