11ebf17c3fd8e3b74523dd6df7c2dd8cafcf4fb4
martinpastore <martinpablopastore@gmail.com>
2018-10-30 16:32:26 -0300

    components and pages

--- module.js
+++ module.js
@@ -6,6 +6,8 @@ burger.module([
     [{name: 'header'},{type: 'component'},{selector: 'p-header'}],
     [{name: 'spinner'},{type: 'component'},{selector: 'p-spinner'}],
     [{name: 'contact-form'},{type: 'component'},{selector: 'p-contact-form'}],
+    [{name: 'accordion'},{type: 'component'},{selector: 'p-accordion'}],
     [{name: 'home'},{type: 'page'},{order: '0'},{route: '/'}],
-    [{name: 'contact'},{type: 'page'},{order: '1'},{route: '/contact'}],
+    [{name: 'contact'},{type: 'page'},{order: '2'},{route: '/contact'}],
+    [{name: 'about'},{type: 'page'},{order: '1'},{route: '/about'}],
 ]);
