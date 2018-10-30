2dac9df7ca2c997abe0e299672098f99dec17559
martinpastore <martinpablopastore@gmail.com>
2018-10-30 14:28:58 -0300

    added new page and integrating routing

--- module.js
+++ module.js
@@ -5,5 +5,7 @@ burger.module([
     [{name: 'footer'},{type: 'component'},{selector: 'p-footer'}],
     [{name: 'header'},{type: 'component'},{selector: 'p-header'}],
     [{name: 'spinner'},{type: 'component'},{selector: 'p-spinner'}],
-    [{name: 'home'},{type: 'page'},{selector: 'p-home'}]
+    [{name: 'contact-form'},{type: 'component'},{selector: 'p-contact-form'}],
+    [{name: 'home'},{type: 'page'},{order: '0'},{route: '/'}],
+    [{name: 'contact'},{type: 'page'},{order: '1'},{route: '/contact'}],
 ]);
