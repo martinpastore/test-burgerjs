c53db4cc2b96381d6b01b973642678344ff69d61
martinpastore <martinpablopastore@gmail.com>
2018-11-01 11:54:12 -0300

    Add tabs and suggestions

--- module.js
+++ module.js
@@ -7,7 +7,9 @@ burger.module([
     [{name: 'spinner'},{type: 'component'},{selector: 'p-spinner'}],
     [{name: 'contact-form'},{type: 'component'},{selector: 'p-contact-form'}],
     [{name: 'accordion'},{type: 'component'},{selector: 'p-accordion'}],
+    [{name: 'tabs'},{type: 'component'},{selector: 'p-tabs'}],
     [{name: 'home'},{type: 'page'},{order: '0'},{route: '/'}],
     [{name: 'contact'},{type: 'page'},{order: '2'},{route: '/contact'}],
     [{name: 'about'},{type: 'page'},{order: '1'},{route: '/about'}],
+    [{name: 'suggestions'},{type: 'page'},{order: '3'},{route: '/suggestions'}],
 ]);
